2b34101702aa7dec99c376fe87d943f8a3e58549
Ruttkay-Nedecky Peter <Peter.Ruttkay-Nedecky@steinringer.sk>
2017-08-01 09:42:49 +0200

    feat: add config

---
+++ index.js
@@ -0,0 +1,45 @@
+module.exports = {
+    env: {
+        'browser': true,
+        'es6': false
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:angular/johnpapa'
+    ],
+    rules: {
+        'indent': 0,
+        'linebreak-style': 0,
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'strict': [
+            'error',
+            'function'
+        ],
+        'no-use-before-define' : 0,
+        'angular/controller-name': [
+            'error',
+            '[a-z][\\w\\.]+Ctrl'
+        ],
+        'angular/file-name': 0,
+        'angular/constant-name': 0,
+        'angular/controller-as-route': 0,
+        'angular/controller-as-vm': 0,
+        'angular/controller-as': 0,
+        'angular/no-controller': 0,
+        'angular/no-inline-template': 0,
+        'angular/no-service-method': 0,
+        'angular/no-services': 0,
+        'angular/prefer-component': 0
+    },
+    globals: {
+        angular: true,
+        _: true
+    }
+};
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Steinringer angular ESlint configuration
+
+ESlint config for angular web development at Steinringer WEB and IT solutions
+
+## Install
+```sh
+npm install --save-dev eslint eslint-config-st-web-ng
+```
+Extend in ```.eslintrc.json``` file 
+```json
+{
+    "extends": "st-web-ng"
+}
+```
